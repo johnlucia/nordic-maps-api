fee394b81abb37da06ed6e2a699abfc1adab41b3
johnlucia <johnmlucia@gmail.com>
2020-03-01 13:01:53 -0800

    no login for api

--- app/controllers/api/v1/welcome_data_controller.rb
+++ app/controllers/api/v1/welcome_data_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class WelcomeDataController < ApplicationController
+      skip_before_action :require_login
+
       def index
         @main_sponsor = Sponsor.main_sponsor
         @secondary_sponsors = Sponsor.secondary_sponsors
