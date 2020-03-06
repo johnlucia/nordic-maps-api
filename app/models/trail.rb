4a1f0ca8950ed7dc329de7569fa6a78f3672e3a4
johnlucia <johnmlucia@gmail.com>
2020-03-06 08:29:23 -0800

    validate da json input

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,22 @@
 class Trail < ApplicationRecord
+  validate :validate_coordinates
+
+  def validate_coordinates
+    begin
+      data = JSON.parse(coordinates)
+      valid = data.kind_of?(Array) && data.length > 2
+    rescue
+      valid = false
+    end
+
+    valid ? true : errors.add(:coordinates, "Must be a valid JSON array")
+  end
+
+  def parsed_coordinates
+    begin
+      JSON.parse(coordinates)
+    rescue
+      return nil
+    end
+  end
 end
