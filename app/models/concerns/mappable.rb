f73c886d17538647254bda331680dea0fdd169bb
johnlucia <johnmlucia@gmail.com>
2020-03-15 21:02:10 -0700

    move some stuff into a mappable concern

---
+++ app/models/concerns/mappable.rb
@@ -0,0 +1,29 @@
+module Mappable
+  extend ActiveSupport::Concern
+
+  included do
+    validate :validate_coordinates
+    before_save :convert_coordinates
+  end
+
+  def coordinate
+    { latitude: latitude, longitude: longitude }
+  end
+
+  def convert_coordinates
+    self.latitude = latitude.to_f
+    self.longitude = longitude.to_f
+  end
+
+  def validate_coordinates
+    valid = false
+    begin
+      lat = latitude.to_f
+      lon = longitude.to_f
+      valid = lat.between?(43,45) && lon.between?(-122, -121)
+    rescue
+      valid = false
+    end
+    valid ? true : errors.add(:coordinates, "Please enter a valid Latitude and Longitude")
+  end
+end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,25 +1,3 @@
 class Shelter < ApplicationRecord
-  validate :validate_coordinates
-  before_save :convert_coordinates
-
-  def coordinate
-    { latitude: latitude, longitude: longitude }
-  end
-
-  def convert_coordinates
-    self.latitude = latitude.to_f
-    self.longitude = longitude.to_f
-  end
-
-  def validate_coordinates
-    valid = false
-    begin
-      lat = latitude.to_f
-      lon = longitude.to_f
-      valid = lat.between?(43,45) && lon.between?(-122, -121)
-    rescue
-      valid = false
-    end
-    valid ? true : errors.add(:coordinates, "Please enter a valid Latitude and Longitude")
-  end
+  include Mappable
 end
