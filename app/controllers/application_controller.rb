e3d03923f20032ab2afb086c36bd4eeaa95cbfad
johnlucia <johnmlucia@gmail.com>
2020-04-18 16:02:58 -0700

    ui help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   def require_login
-    http_basic_authenticate_or_request_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PWD']
+    if http_basic_authenticate_or_request_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PWD']
+      @password_protected = true
+    end
   end
 
 end
