c657b8414aa94afd93a3704bdf19b38ecd176b7a
johnlucia <johnmlucia@gmail.com>
2020-04-18 16:03:27 -0700

    trail type migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_005018) do
+ActiveRecord::Schema.define(version: 2020_04_18_230028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_03_21_005018) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
     t.boolean "groomed"
+    t.string "trail_type"
   end
 
   create_table "welcome_contents", force: :cascade do |t|
