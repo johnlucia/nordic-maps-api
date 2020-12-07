98afbb6b2b438c6f3f9563fa54f9a02cac3dd19b
John Lucia <johnmlucia@gmail.com>
2020-12-06 16:42:38 -0800

    use timezone in initialize too

--- app/services/UsageTracker.rb
+++ app/services/UsageTracker.rb
@@ -1,19 +1,25 @@
 class UsageTracker
   TIME_ZONE = "Pacific Time (US & Canada)"
+
   def self.count_homescreen_load
     new.count_homescreen_load
   end
 
   def self.homescreen_load_count(total_days=10)
     screen_view_data = {}
-    today = Time.now.in_time_zone(TIME_ZONE).to_date
-    Date.today.downto(today - total_days.days) do |date|
+
+    Date.today.downto(self.today - total_days.days) do |date|
       screen_view_data[date.to_s] = new(date).todays_homescreen_views
     end
     screen_view_data
   end
 
-  def initialize(date=Date.today)
+  def self.today
+    Time.now.in_time_zone(TIME_ZONE).to_date
+  end
+
+  def initialize(date=nil)
+    date ||= self.class.today
     @day = date.to_s
   end
 
