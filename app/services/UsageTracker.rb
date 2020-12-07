f6ccc8e29ce2fef92605dde3fd21ac2ac9507738
John Lucia <johnmlucia@gmail.com>
2020-12-06 16:31:23 -0800

    use pacific time

--- app/services/UsageTracker.rb
+++ app/services/UsageTracker.rb
@@ -1,12 +1,13 @@
 class UsageTracker
-
+  TIME_ZONE = "Pacific Time (US & Canada)"
   def self.count_homescreen_load
     new.count_homescreen_load
   end
 
   def self.homescreen_load_count(total_days=10)
     screen_view_data = {}
-    Date.today.downto(Date.today - total_days.days) do |date|
+    today = Time.now.in_time_zone(TIME_ZONE).to_date
+    Date.today.downto(today - total_days.days) do |date|
       screen_view_data[date.to_s] = new(date).todays_homescreen_views
     end
     screen_view_data
