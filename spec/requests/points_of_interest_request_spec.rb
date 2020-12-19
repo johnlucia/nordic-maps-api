078d903d80f62bc2599212f0b779bab050d65f8a
John Lucia <johnmlucia@gmail.com>
2020-12-19 15:13:49 -0800

    request spec too

--- spec/requests/points_of_interest_request_spec.rb
+++ spec/requests/points_of_interest_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "PointsOfInterests", type: :request do
-
+  describe "GET /points_of_interest" do
+    it "requires a password" do
+      get shelters_path
+      expect(response).to have_http_status(401)
+    end
+  end
 end
