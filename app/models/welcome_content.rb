edd6ab73f91f5d3e6560d039a09af7d036c469e0
John Lucia <johnmlucia@gmail.com>
2021-02-14 15:23:10 -0800

    uri validation on content links

--- app/models/welcome_content.rb
+++ app/models/welcome_content.rb
@@ -1,3 +1,13 @@
 class WelcomeContent < ApplicationRecord
+  validate :has_valid_href?
+
   scope :active_content, -> { where(active: true).order(:position) }
+
+  def has_valid_href?
+    return true if link_url.blank?
+    
+    uri = URI.parse link_url
+    valid = [URI::HTTP, URI::HTTPS, URI::MailTo].include? uri.class
+    valid || errors.add(:link_url, "Link URL not valid. Valid formats include: `http://www.link.com`, `https://link.com`, and `mailto:bob@gmail.com`")
+  end
 end
